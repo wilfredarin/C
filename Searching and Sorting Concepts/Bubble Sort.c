a82b26bb42953430b695539b253c153fbdacc1bd
sameer ranjan <40149707+wilfredarin@users.noreply.github.com>
2018-12-17 10:16:17 +0530

    Create Bubble Sort.c
    keeps Swapping element! bubbles up the largest element in the array;
    Swap | sort | printArray | main()

---
+++ Searching and Sorting Concepts/Bubble Sort.c
@@ -0,0 +1,65 @@
+/*Bubble Sort
+
+
+what happens!!!
+
+Bubble Sort is the simplest sorting algorithm that works by repeatedly 
+swapping the adjacent elements if they are in wrong order.
+Example:
+First Pass:
+( 5 1 4 2 8 ) –> ( 1 5 4 2 8 ), Here, algorithm compares the first two elements, and swaps since 5 > 1.
+( 1 5 4 2 8 ) –>  ( 1 4 5 2 8 ), Swap since 5 > 4
+( 1 4 5 2 8 ) –>  ( 1 4 2 5 8 ), Swap since 5 > 2
+( 1 4 2 5 8 ) –> ( 1 4 2 5 8 ), Now, since these elements are already in order (8 > 5), algorithm does not swap them.
+
+
+*/
+
+// C program for implementation of Bubble sort 
+#include <stdio.h> 
+
+
+///swap!
+void swap(int *xp, int *yp) 
+{ 
+	int temp = *xp; 
+	*xp = *yp; 
+	*yp = temp; 
+} 
+
+// array length of array
+//first for loop : for passes
+//second swap till last - no of passses
+// bigest element bubbles up in each pass one bigest is at ryt place
+//right most part is sorted always
+void bubbleSort(int arr[], int n) 
+{ 
+int i, j; 
+for (i = 0; i < n-1; i++)
+//first pass	 
+	//biggest element bubbles up in each iteration...so dont need to go till last.
+	// Last i elements are already in place 
+	for (j = 0; j < n-i-1; j++) 
+		if (arr[j] > arr[j+1]) 
+			swap(&arr[j], &arr[j+1]); 
+} 		//compare all 	
+
+/* Function to print an array */
+void printArray(int arr[], int size) 
+{ 
+	int i; 
+	for (i=0; i < size; i++) 
+		printf("%d ", arr[i]); 
+	printf("n"); 
+} 
+
+// Driver program to test above functions 
+int main() 
+{ 
+	int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+	int n = sizeof(arr)/sizeof(arr[0]); 
+	bubbleSort(arr, n); 
+	printf("Sorted array: \n"); 
+	printArray(arr, n); 
+	return 0; 
+} 
